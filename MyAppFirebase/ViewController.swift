91beeafb86b0083f4d55ee8357f6767728a6e129
Avinash Kaul <avinashkaul.0191@gmail.com>
2019-09-28 17:44:57 +0530

    Initial Commit

---
+++ MyAppFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAppFirebase
+//
+//  Created by Avinash Kaul on 28/09/19.
+//  Copyright © 2019 Avinash Kaul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
